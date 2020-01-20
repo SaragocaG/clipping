1abb9b4a0286e30e4e92af69b14ea71a08848183
SARAGOCA Glaydson <glaydson.saragoca@gmail.com>
2020-01-19 23:57:23 -0300

    header

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,34 @@
 <template>
-  <div>Header</div>
+  <header class="pt-5">
+    <div class="container py-5">
+      <h1 id="headerCaption" class="display-4">
+        Cursos
+      </h1>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  header {
+    background-image: url('/images/headerBg.svg');
+    background-position-y: center;
+    background-size: cover;
+  }
+  h1 {
+    font-weight: bolder;
+    color: white;
+  }
+  #headerCaption {
+    text-shadow: 2px 2px rgb(50, 50, 50);
+  }
 </style>
